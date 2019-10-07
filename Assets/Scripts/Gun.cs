4eb5b03b37e69b99c041004ce51d2d4432c2b5f6
Humza Khan <humza.khan@msn.com>
2019-10-07 16:23:33 -0400

    Added gunfire scripts and animations + Reticles

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float fireRate;
+    protected float lastFireTime;
+
+    void Start()
+    {
+        lastFireTime = Time.time - 10;
+    }
+    
+    protected virtual void Update()
+    {
+        
+    }
+
+    protected void Fire()
+    {
+        GetComponentInChildren<Animator>().Play("Fire");
+    }
+}

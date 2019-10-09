52889320ce9612be8a8ac4ddb520a908464d0d6c
Humza Khan <humza.khan@msn.com>
2019-10-09 18:35:16 -0400

    Added Main Menu scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // loads the Battle scene
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+    // exits the app
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}

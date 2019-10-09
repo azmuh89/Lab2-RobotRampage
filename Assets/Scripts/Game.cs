0f71312e167e58141071130827470915914e8572
Humza Khan <humza.khan@msn.com>
2019-10-09 16:14:24 -0400

    Added Spawns

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+
+    [SerializeField]
+    RobotSpawn[] spawns;
+
+    public int enemiesLeft;
+
+    // Initialize the singleton and call SpawnRobots()
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+    // Go through each RObotSpawn in array and cal SpawnRobot() to actually spawn robot
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PickupSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pickups;
+
+    // Instantiates a random pickup and sets its position to that of the PickupSpawn GameObject
+    void spawnPickup()
+    {
+        // Instantiate a random pickup
+        GameObject pickup = Instantiate(pickups[Random.Range(0, pickups.Length)]);
+        pickup.transform.position = transform.position;
+        pickup.transform.parent = transform;
+    }
+    // Waits 20 seconds before calling spawnPickup()
+    IEnumerator respawnPickup()
+    {
+        yield return new WaitForSeconds(20);
+        spawnPickup();
+    }
+    // Spawns a pickup as soon as the game begins
+    void Start()
+    {
+        spawnPickup();
+    }
+    // Starts the coroutine to respawn when the player has pickup up something
+    public void PickupWasPickedUp()
+    {
+        StartCoroutine("respawnPickup");
+    }
+    
+    void Update()
+    {
+        
+    }
+}
